a11bf7621d2ddc6b039207e67a5c2b4549cb53b1
marisasaunders <marisaksaunders@gmail.com>
2022-01-16 17:09:50 -0500

    commit tree ffe7cb661999bd6e2a8372df59d7656e9cfe6bbd

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record) {
+    let item = record.find(obj => obj.result === "W")
+    if (item) {
+        return item.year
+    }
+}
+
+//use below to return result in replit
+//superbowlWin(record);
